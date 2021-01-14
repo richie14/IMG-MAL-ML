bd4fb403a4ef8ead0ace7c30f229ac0af2f5d676
richie14 <richab1497@gmail.com>
2021-01-15 03:19:14 +0530

    Add files via upload
    14Jan2021 update- Multiclass classifier

---
+++ MultiClass.py
@@ -0,0 +1,62 @@
+# import pandas
+import pandas as pd
+
+# read the dataset
+df = pd.read_csv('sample2.csv')
+
+df.head()
+
+
+print(df.shape)
+
+num_of_classes = len(df.Label.unique())
+#print(num_of_classes)
+
+#df.describe()
+
+
+# split train input and output data
+#X = df.drop(axis=0, columns=['Label', 'Case #'])
+X = df.drop(axis=0, columns=['Label'])
+Y = df.Label
+
+#Print the shape of X and Y
+#print(X.shape)
+#print(Y.shape)
+
+
+from sklearn.model_selection import train_test_split
+
+# Split into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+
+from xgboost import XGBClassifier
+
+# Create a classifier
+xgb = XGBClassifier(booster='gbtree', objective='multi:softprob', use_label_encoder=False, random_state=42, eval_metric="auc", num_class=num_of_classes)
+
+# Fit the classifier with the training data
+xgb.fit(X_train,y_train)
+
+
+
+from sklearn.metrics import roc_auc_score
+from sklearn import preprocessing
+
+# Use trained model to predict output of test dataset
+val = xgb.predict(X_test)
+
+lb = preprocessing.LabelBinarizer()
+lb.fit(y_test)
+
+y_test_lb = lb.transform(y_test)
+val_lb = lb.transform(val)
+
+print("MultiClass Classifier:")
+print(roc_auc_score(y_test_lb, val_lb, average='macro'))
+
+
+output = pd.DataFrame()
+output['Expected Output'] = y_test
+output['Predicted Output'] = val
+output.head()
